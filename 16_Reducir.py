152a4ff26bfcfd7ae18819a5ce8c492046dc3da7
jlopez00001 <jjuliolopezm1612@gmail.com>
2023-10-24 15:56:37 -0600

    16_Reducir.py

---
+++ 16_Reducir.py
@@ -0,0 +1,51 @@
+#+++++++++++++++++++++++++++++++++++++++++++++++++++
+# JOSE JULIO LOPEZ MARQUEZ 
+#++++++++++++++++++++++++++++++++++++++++++++++++++
+# Paradigmas de programación 
+# Matemática Algoritmica 
+# ESFM IPN Octubre 2023
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++
+# USO de reducciones (colapsar resultados )
+#+++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#+++++++++++++++++++++++++++++++++++++++++++++++
+# Multiplicación de todos los números en la lista
+#+++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from functools import reduce 
+
+bigdata = [2,3,5,7,11,13,17,19,23,29]
+
+#++++++++++++++++
+# Función x*y
+#++++++++++++++
+multiplicar = lambda x,y: x*y
+
+print (reduce (multiplicar, bigdata))
+print (reduce (suma, bigdata))
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Reduce le aplica la función por pares a los resultados y 
+# el siguiente elemento comenzando con los dos primeros
+# elementos.
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
