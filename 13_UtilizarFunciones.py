1a5f95a533acff945e53b2b04e00ff9398bfd36c
jlopez00001 <143755746+jlopez00001@users.noreply.github.com>
2023-11-17 13:08:27 -0600

    Add files via upload

---
+++ 13_UtilizarFunciones.py
@@ -0,0 +1,14 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# JOSE JULIO LOPEZ MARQUEZ 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#   Paradigmas de programación
+#   Matemática Algoritmica
+#   ESFM-IPN    NOV_2023
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#++++++++++++++++++++++++++++++++++++++++
+# De fuciones.py traer las funciones suma y promedio
+#+++++++++++++++++++++++++++++++++++++++++++++++
+from funciones import suma, promedio
+print (suma(100,200))
+print (promedio(100,200))
---
+++ funciones.py
@@ -0,0 +1,17 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# JOSE JULIO LOPEZ MARQUEZ 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#   Paradigmas de programación
+#   Matemática Algoritmica
+#   ESFM-IPN    NOV_2023
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#+++++++++++++++++++++++++++++++++++
+# Funciones suma, promedio y potencia
+#+++++++++++++++++++++++++++++++++++++
+def suma (x,y):
+      return x+y
+def promedio(x,y):
+      return 0.5*(x+y)
+def potencia(x,y):
+      return x**y
