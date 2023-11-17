07f7c111119f5da6e9163012d381eefb8f66b31c
jlopez00001 <143755746+jlopez00001@users.noreply.github.com>
2023-11-17 12:52:22 -0600

    Add files via upload

---
+++ 11_zspline.py
@@ -0,0 +1,56 @@
+#+++++++++++++++++++++++++++++++++++++++++++
+# JOSE JULIO LOPEZ MARUEZ 
+#++++++++++++++++++++++++++++++++++
+#   Paradigmas de programación
+#   Matemática Algoritmica
+#   ESFM-IPN    NOV_2023
+#++++++++++++++++++++++++++++++++++++++
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#   Curvas Z-splines en n dimensiones
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import numpy as np
+from curva import Curva,zspline
+import matplotlib.pyplot as plt
+import math
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#   Conjunto de puntos
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#Numero de puntos
+nump:np.int32 = 5
+
+# Dimensión
+dim:np.int32 = 2
+
+# Memoria para puntos
+puntos = np.zeros(dim*nump,dtype=np.float64)
+
+#Parametrización
+X = np.linspace(0.0,2*np.pi,nump+1)
+
+#Coordenada x
+puntos[0:nump] = np.cos(X[0:nump]) + 0.0*np.sin(2*X[0:nump])
+
+#Coordenada y
+puntos[nump:2*nump] = np.sin(X[0:nump]) + 0.0*np.sin(2*X[0:nump])
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#   Curva Z-spline de n puntos zspline(p,d,n,m)
+#   p: puntos, d: dimension, n: segmentos de curva, m: continuidad
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+n:np.int32 = 1000
+x1,y1 = zspline(puntos,dim,n,2)
+x2,y2 = zspline(puntos,dim,n,1)
+x3,y3 = zspline(puntos,dim,n,0)
+plt.plot(x3,y3, lw=3,color="orange")
+plt.plot(x2,y2, lw=3,color="red")
+plt.plot(x1,y1, lw=3,color="blue")
+plt.scatter(puntos[0:nump],puntos[nump:2*nump],marker='o' ,color='black')
+plt.xlabel("Coordenada x")
+plt.ylabel("Coordenada y")
+plt.title("Curva cerrada Z-spline en 2D")
+plt.show()
