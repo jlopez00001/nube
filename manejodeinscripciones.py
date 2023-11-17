3ac9928a7546eb4dc5dcacc12b2801a17b3b5956
jlopez00001 <143755746+jlopez00001@users.noreply.github.com>
2023-11-17 13:52:24 -0600

    Add files via upload

---
+++ manejodeinscripciones.py
@@ -0,0 +1,31 @@
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++
+# JOSÉ JULIO LOPEZ MARQUEZ 
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++
+#   Paradigmas de programación
+#   Matemática Algoritmica
+#   ESFM-IPN    NOV_2023
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from aplicacion.modelos.usuario import Usuario
+from aplicacion.repositorio.repositoriodeusuarios import RepositorioDeUsuarios
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++
+#   Clase ManejoDeInscripciones
+#   NO TIENE VARIABLES!!!
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class ManejoDeInscripciones:
+    #+++++++++++++++++++++++++++++++++++++++++++++++++++++
+    #   Decorador staticmethod
+    #   el objeto sólo tiene la función iscribirUsuario
+    #   ENVUELVE LA FUNCION SIN LLAMAR VARIABLES LOCALES
+    #   el objeto ManejoDeInscripciones es la interface intercambiable
+
+
+    @staticmethod
+    def inscribirUsuario(usuario: Usuario, repositorioDeUsuarios: RepositorioDeUsuarios) -> None:
+        print("------> Guardando usuario ... \n")
+        repositorioDeUsuarios.abrir()
+        repositorioDeUsuarios.guardar(usuario)
+        repositorioDeUsuarios.cerrar()
+
---
+++ usuario.py
@@ -0,0 +1,40 @@
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++
+# JOSÉ JULIO LOPEZ MARQUEZ 
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++
+#   Paradigmas de programación
+#   Matemática Algoritmica
+#   ESFM-IPN    NOV_2023
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++
+#   Clase usuario
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++
+class Usuario:
+    __nombre: str
+    __apellido: str
+    __edad: int
+
+    #+++++++++++++++++++++++++++++++++++++++++++++++++++++
+    #   Constructor
+    #+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    def __init__(mi, nombre: str, apellido: str, edad: int):
+        mi.__nombre = nombre
+        mi.__apellido = apellido
+        mi.__edad = edad
+
+
+    #+++++++++++++++++++++++++++++++++++++++++++++++++++++
+    #   Getters
+    #+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    def getNombre(mi) -> str:
+        return mi.__nombre
+
+    def getApellido(mi) -> str:
+        return mi.__apellido
+
+    def getEdad(mi) -> int:
+        return mi.__edad
+
+
